870d947411805a8d08b98126e325697c84a57366
prody-dev <rodriguez.christian.dev@gmail.com>
2025-07-04 12:06:42 -0700

    staticfiles config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,7 +126,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR/ 'static',
+                    ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Directorio donde se almacenan los archivos estáticos en producción
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
