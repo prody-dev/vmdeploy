7b7398dbaadba061d64337db0f1cb4af31dda4f1
Ubuntu <prody@VirtualMachine.gafd1rm1gvleln3hjor2pmvgdb.dx.internal.cloudapp.net>
2025-07-04 22:43:42 +0000

    despliegue funcionando

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-esb988poco_b#gfe2_3t*u(gzkqc=f*+c$pv8=+m$jf$95!rm_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['172.184.99.83',]
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*','localhost', '127.0.0.1','172.184.99.83']
 
 
 # Application definition
